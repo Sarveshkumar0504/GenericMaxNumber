7a323d852eee180666e4c16d9fafeef1cae2afd1
sarvesh <sarveshkumar452000@gmail.com>
2022-02-28 19:09:30 -0800

    Interger Number Added

--- GenericMaxNumber/MaxNumber.cs
+++ GenericMaxNumber/MaxNumber.cs
@@ -8,5 +8,14 @@ namespace GenericMaxNumber
 {
     internal class MaxNumber
     {
+        public static void FindMaxNum(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num2) >= 0)
+                Console.WriteLine("Max Value is {0}", num1);
+            else if (num2.CompareTo(num3) >= 0)
+                Console.WriteLine("Max Value is {0}", num2);
+            else
+                Console.WriteLine("Max Value is {0}", num3);
+        }
     }
 }
--- GenericMaxNumber/Program.cs
+++ GenericMaxNumber/Program.cs
@@ -10,6 +10,16 @@ namespace GenericMaxNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Generics Max Number");
+
+            Console.WriteLine("Test Case 1:" + TestCases.GetString(TestCases.IntCase1));
+            MaxNumber.FindMaxNum(TestCases.IntCase1[0], TestCases.IntCase1[1], TestCases.IntCase1[2]);
+            Console.WriteLine("Test Case 2:" + TestCases.GetString(TestCases.IntCase2));
+            MaxNumber.FindMaxNum(TestCases.IntCase2[0], TestCases.IntCase2[1], TestCases.IntCase2[2]);
+            Console.WriteLine("Test Case 3:" + TestCases.GetString(TestCases.IntCase3));
+            MaxNumber.FindMaxNum(TestCases.IntCase3[0], TestCases.IntCase3[1], TestCases.IntCase3[2]);
+            Console.ReadKey();
         }
+    
     }
 }
